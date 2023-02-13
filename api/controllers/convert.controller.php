c04908544ffaf0a1496d1752781a59f8583a74fb
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-13 21:07:13 +0100

    Added the dummmy file upload API method

--- api/controllers/convert.controller.php
+++ api/controllers/convert.controller.php
@@ -72,5 +72,63 @@ final class Base_Converter
 			return json_encode($this->data_array);
 		}
 	}
+
+
+	#create a method to handle the file
+	public function handle_file(){
+		$this->upload_file_name = $this->product_img["name"];
+		#reference the file upload folder
+		$this->upload_dir= "../images/";
+		#create a file size constant
+		#this is 1mb, the max amount of file size we can accept
+		$this->max_file_size=1000000;
+		
+		$this->striped_file=explode('.',$this->upload_file_name);
+		#create an array for the possible type of file allowed for upload
+		$this->legit_file_extension=array("png","jpeg","jpg","gif");
+		$this->legit_upload_file_ext=strtolower(end($this->striped_file));
+		#explode the file name to check for the extension
+		
+		$this->upload_tmp_name=$this->product_img["tmp_name"];
+		$this->upload_file_error=$this->product_img["error"];
+		$this->upload_file_size=$this->product_img["size"];
+		#the file is not uploaded by default
+		$this->file_match_success=false;
+
+		#rename the file
+		$this->randomNumber=rand(0,10000000);
+		$this->randomTime=time();
+		$this->processed_file_name=$this->randomNumber.$this->randomTime.'.'.$this->legit_upload_file_ext;
+		$this->target_file_dir=$this->upload_dir.$this->processed_file_name;
+		#check if there was an error uploading the file
+		if($this->upload_file_error != 0){
+			return 'an error occured while processing file';
+		}else{
+
+			#check if the uploaded file is above the legit file size
+			if($this->upload_file_size > $this->max_file_size){
+				return 'whoza, file size too large';
+			}else{
+
+				#check if the uploaded file matches the legit file extension
+				if(in_array($this->legit_upload_file_ext,$this->legit_file_extension)){
+					#the file matches all the required properties
+					$this->file_match_success=true;
+
+				
+				
+				}else{
+					$this->file_match_success= false;
+
+					return 'invalid file extension, use (png, jpg, gif, jpeg)';
+					
+				}
+			}
+
+		}
+
+		return $this->file_match_success;
+	}
+
 	
 }
