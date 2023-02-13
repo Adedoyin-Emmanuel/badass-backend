8229dbb164f9bdd1cf34f39ad344007dadda0b02
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-13 14:11:08 +0100

    Bug fix

--- api/controllers/convert.controller.php
+++ api/controllers/convert.controller.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace controller\convert_controller;
 
 /**
  * Base class controller for the convert API
@@ -7,14 +8,12 @@
  * */
 
 
-namespace controller\convert_controller;
 
 final class Base_Converter
 {
-	private $API_KEY;
-	private $data_array;
-	private $api_key_check;
-	private $API_KEY;
+	protected $API_KEY;
+	protected $data_array;
+	protected $api_key_check;
 	public  $valid_key;
 
 
@@ -29,63 +28,5 @@ final class Base_Converter
 		return $this->API_KEY;
 	}
 
-	public function check_api_credentials($app_id)
-	{
-		$this->api_key_check = $this->get_api_key();
-		
-		if(empty($app_id) OR !isset($app_id))
-		{
-			$this->data_array = [
-				"data"  => "No key specified",
-				"code" => 403
-			];
-
-			return json_encode($this->data_array);
-		}
-		else if($this->api_key_check != $app_id)
-		{
-			$this->data_array = [
-				"data" => "Invalid API key",
-				"code" => 403
-			]
-
-			return json_encode($this->data_array);
-		}else if($this->api_key_check == $app_id)
-		{
-			$this->valid_key = true;
-
-			 $this->data_array = [
-	            "data" => "Hello World",
-	            "code" => "200"
-       		 ];
-
-       		 return json_encode($this->data_array);
-
-		}
-		else
-		{
-			$this->data_array = [
-	            "data" => "An Error Has Occurred",
-	            "code" => 500
-        	];
-
-        	return json_encode($this->data_array);
-
-		}
-
-	}
-
-
-	public function process_file_upload($file)
-	{
-
-		if(empty($file) OR !isset($file))
-		{
-			return "please upload a file";
-			die();
-		}
-
-		$this->file_uploaded = $file;
-
-	}
+	
 }
--- api/convert/index.php
+++ api/convert/index.php
@@ -6,9 +6,7 @@ require_once "./../controllers/convert.controller.php";
 use controller\convert_controller\Base_Converter;
 
 $converter = new Base_Converter();
-$api_request = $_REQUEST["app_id"];
 
-$converter->check_api_credentials($api_request);
-
+echo $converter->get_api_key();
 
 ?>
