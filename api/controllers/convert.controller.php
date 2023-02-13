113411a9b4f67afc0c496ec81bf63a142de00f97
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-13 20:18:21 +0100

    Implemented the API check

--- api/controllers/convert.controller.php
+++ api/controllers/convert.controller.php
@@ -11,9 +11,9 @@ namespace controller\convert_controller;
 
 final class Base_Converter
 {
-	protected $API_KEY;
-	protected $data_array;
-	protected $api_key_check;
+	private $API_KEY;
+	private $data_array;
+	private $api_key_check;
 	public  $valid_key;
 
 
@@ -21,6 +21,7 @@ final class Base_Converter
 	{
 		$this->API_KEY = "d847b2e0-14f9-11e9-b5dc-0242ac130003";
 		$this->valid_key = false;
+		$this->data_array = [];
 	}
 
 	public function get_api_key () 
@@ -28,5 +29,48 @@ final class Base_Converter
 		return $this->API_KEY;
 	}
 
+	public function check_app_credentials ($app_id)
+	{
+		if(empty($app_id) OR !isset($app_id))
+		{
+			$this->data_array = [
+					"data" => "App key not specified!",
+					"code" => 403
+			];
+
+			return json_encode($this->data_array);
+		}
+		else if($app_id != $this->API_KEY)
+		{
+			$this->data_array = [
+					"data" => "Invalid API key",
+					"code" => 403
+			];
+
+			return json_encode($this->data_array);
+
+		}
+		else if($app_id == $this->API_KEY)
+		{
+			//correct API key
+			$this->data_array = [
+					"data" => "Valid API key",
+					"code" => 200
+			];
+
+			$this->valid_key = true;
+
+			return json_encode($this->data_array);
+		}
+
+		else{
+			$this->data_array = [
+					"data" => "An unknown error occured",
+					"code" => 500
+			];
+
+			return json_encode($this->data_array);
+		}
+	}
 	
 }
--- api/convert/index.php
+++ api/convert/index.php
@@ -5,8 +5,21 @@ require_once "./../controllers/convert.controller.php";
 
 use controller\convert_controller\Base_Converter;
 
-$converter = new Base_Converter();
+const CONVERTER_CONTROLLER = new Base_Converter();
 
-echo $converter->get_api_key();
+$USER_APP_ID  = $_REQUEST["app_id"];
+
+$SERVER_RESPONSE = CONVERTER_CONTROLLER->check_app_credentials($USER_APP_ID);
+
+//check if the API key is legit
+if(!CONVERTER_CONTROLLER->valid_key)
+{
+	echo $SERVER_RESPONSE;
+}
+
+
+
+
+
 
 ?>
