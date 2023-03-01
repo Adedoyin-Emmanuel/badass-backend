bab0641ef045662bbe0347a3a8fb1deccd5f50bf
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-01 22:57:15 +0100

    Bug fix

--- api/convert/index.php
+++ api/convert/index.php
@@ -4,7 +4,10 @@ require_once "./../controllers/convert.controller.php";
 
 const CONVERT_FILE_CONTROLLER = new File_Converter();
 
-$USER_APP_ID  = @$_REQUEST["app_id"];
+if(isset($_REQUEST["app_id"]))
+{
+	$USER_APP_ID  = @$_REQUEST["app_id"];
+}
 
 $SERVER_RESPONSE = CONVERT_FILE_CONTROLLER->check_app_credentials($USER_APP_ID);
 
