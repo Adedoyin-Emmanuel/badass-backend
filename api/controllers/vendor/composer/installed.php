d5d096fe5bfd47a57667c6e1a85c1ddb3f8c7246
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-06 01:53:22 +0100

    Added the convertio's API and removed unused modules

---
+++ api/controllers/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b9e53443ea06f90ca4afa0e2b5f6daa1049abae8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b9e53443ea06f90ca4afa0e2b5f6daa1049abae8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'convertio/convertio-php' => array(
+            'pretty_version' => 'v0.4',
+            'version' => '0.4.0.0',
+            'reference' => 'aabb3538e4ceb7db746614f7cfb01c7a91ce755f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../convertio/convertio-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
