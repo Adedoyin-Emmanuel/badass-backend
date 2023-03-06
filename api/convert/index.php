7c1848c04b3bcdb183de3bc2633737d4d314d34b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-06 01:22:48 +0100

    Changed PHP version, constant bug fix

--- api/convert/index.php
+++ api/convert/index.php
@@ -2,21 +2,21 @@
 
 require_once "./../controllers/convert.controller.php";
 
-const CONVERT_FILE_CONTROLLER = new File_Converter();
+$CONVERT_FILE_CONTROLLER = new File_Converter();
 
 $USER_APP_ID  = @$_REQUEST["app_id"];
 
-$SERVER_RESPONSE = CONVERT_FILE_CONTROLLER->check_app_credentials($USER_APP_ID);
+$SERVER_RESPONSE = @$CONVERT_FILE_CONTROLLER->check_app_credentials($USER_APP_ID);
 
 //check if the API key is legit
-if(!CONVERT_FILE_CONTROLLER->valid_key)
+if(!$CONVERT_FILE_CONTROLLER->valid_key)
 {
 	echo $SERVER_RESPONSE;
 }
 
 if(isset($_REQUEST["convert_to"]) AND isset($_FILES["files"]))
 {
-	echo(CONVERT_FILE_CONTROLLER->check_file_request_sent("files", $_REQUEST["convert_to"]));
+	echo($CONVERT_FILE_CONTROLLER->check_file_request_sent("files", $_REQUEST["convert_to"]));
 }
 
 ?>
